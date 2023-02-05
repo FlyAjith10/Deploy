a447d9ba9a41f0d317fb57cad44b5be71de308ec
FlyAjith10 <124163206+FlyAjith10@users.noreply.github.com>
2023-02-06 03:43:11 +0530

    build
    easy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-cd /var/www/html/<project name>
+cd /var/www/html/
 
 if [ "$APPLICATION_NAME" == "<name of CodeDeploy application used for production deploys>" ]
 then
