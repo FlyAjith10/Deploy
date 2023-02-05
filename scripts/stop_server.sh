708c4d77a197aeeb108e531813df0abf2bcb58a8
FlyAjith10 <124163206+FlyAjith10@users.noreply.github.com>
2023-02-06 03:45:05 +0530

    enjoy
    checkout

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-cd /var/www/html/<project name>
-pm2 stop "<project name>"
-pm2 delete "<project name>"
+cd /var/www/html/
+pm2 stop "html"
+pm2 delete "html"
