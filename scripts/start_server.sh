37fdecb74c1761a692a9b1f4ac95a8e3b593791a
root <root@ip-172-31-5-65.ap-south-1.compute.internal>
2023-02-05 21:22:30 +0000

    first

---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+
+rt NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+cd /var/www/html/<project name>
+pm2 start npm --name "<project name>" -- run dev
