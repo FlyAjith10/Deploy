5ca8cb958f8d3507517a02dc75879da7c186331f
FlyAjith10 <124163206+FlyAjith10@users.noreply.github.com>
2023-02-06 03:44:10 +0530

    go
    free

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@ rt NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-cd /var/www/html/<project name>
-pm2 start npm --name "<project name>" -- run dev
+cd /var/www/html/
+pm2 start npm --name "html" -- run dev
